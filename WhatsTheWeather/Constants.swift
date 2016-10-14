da5cd933b43795181bfb6b89df437e3c2f799879
Brizton-Floyd <briztonfloyd@briztons-mbp.attlocal.net>
2016-10-14 16:22:10 -0700

    Changed API Connection String

--- WhatsTheWeather/Constants.swift
+++ WhatsTheWeather/Constants.swift
@@ -8,5 +8,5 @@
 
 import Foundation
 
-let WEATHER_URL = "https://api-ak-aws.wunderground.com/api/8dee7a0127c63e7f/forecast10day/hourly10day/astronomy10day/geolookup/alerts/conditions/lang:EN/units:english/v:2.0/q/\(LocationCoordinates.sharedInstance.lattitude),\(LocationCoordinates.sharedInstance.longitude).json?ttl=300"
+let WEATHER_URL = "https://api-ak-aws.wunderground.com/api/8dee7a0127c63e7f/forecast10day/hourly10day/astronomy10day/geolookup/alerts/conditions/lang:EN/units:english/v:2.0/q/\(LocationCoordinates.sharedInstance.lattitude!),\(LocationCoordinates.sharedInstance.longitude!).json?ttl=300"
 
